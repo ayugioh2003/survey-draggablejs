588315408edb42cc485fe0d565457935bb29c841
John Chiu <john@makertec.net>
2022-08-01 15:56:28 +0800

    feat: header

---
+++ src/components/Layout/LayoutHeader.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, useRouter } from 'vue-router'
+</script>
+
+<template>
+  <header class="bg-green-500 py-2">
+    <div class="container mx-auto">
+      <nav class="flex gap-2">
+        <template v-for="route in useRouter().options.routes" :key="route.name">
+          <RouterLink :to="route.path" class="capitalize text-white">{{
+            route.name
+          }}</RouterLink>
+        </template>
+      </nav>
+    </div>
+  </header>
+</template>
