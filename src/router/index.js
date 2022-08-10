dfadc86b72f5a1f2e7ccca0de0c852a2c3820b5d
John Chiu <john@makertec.net>
2022-08-10 17:01:35 +0800

    fix: route use @

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -12,27 +12,27 @@ const router = createRouter({
     {
       path: '/demo1Draggable',
       name: 'demo1Draggable',
-      component: () => import('../views/Demo1DraggableView.vue'),
+      component: () => import('@/views/Demo1DraggableView.vue'),
     },
     {
       path: '/demo2Droppable',
       name: 'demo2Droppable',
-      component: () => import('../views/Demo2DroppableView.vue'),
+      component: () => import('@/views/Demo2DroppableView.vue'),
     },
     {
       path: '/demo3Plugins',
       name: 'demo3Plugins',
-      component: () => import('../views/Demo3PluginsView.vue'),
+      component: () => import('@/views/Demo3PluginsView.vue'),
     },
     {
       path: '/demo4Sortable',
       name: 'demo4Sortable',
-      component: () => import('../views/Demo4SortableView.vue'),
+      component: () => import('@/views/Demo4SortableView.vue'),
     },
     {
       path: '/demo5Swappable',
       name: 'demo5Swappable',
-      component: () => import('../views/Demo5SwappableView.vue'),
+      component: () => import('@/views/Demo5SwappableView.vue'),
     },
     {
       path: '/about',
@@ -40,7 +40,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('@/views/AboutView.vue'),
     },
   ],
 })
