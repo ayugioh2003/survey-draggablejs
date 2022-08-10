5939272f6db17bb3f67bfc13fa941812f3769592
John Chiu <john@makertec.net>
2022-08-10 16:54:19 +0800

    fix: deploy.sh

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  // const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), '')
 
   return defineConfig({
     plugins: [vue()],
@@ -17,6 +17,6 @@ export default ({ mode }) => {
     server: {
       host: true,
     },
-    // base: env.NODE_ENV === 'production' ? '/survey-draggablejs/' : './',
+    base: env.NODE_ENV === 'production' ? '/survey-draggablejs/' : './',
   })
 }
