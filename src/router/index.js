e79462941c7b6ae6b32bbf6f10276d4ae53a922d
John Chiu <john@makertec.net>
2022-08-10 17:11:23 +0800

    fix: dynamic import vite error

--- src/router/index.js
+++ src/router/index.js
@@ -12,27 +12,32 @@ const router = createRouter({
     {
       path: '/demo1Draggable',
       name: 'demo1Draggable',
-      component: () => import('@/views/Demo1DraggableView.vue'),
+      component: () =>
+        import(/* @vite-ignore */ '@/views/Demo1DraggableView.vue'),
     },
     {
       path: '/demo2Droppable',
       name: 'demo2Droppable',
-      component: () => import('@/views/Demo2DroppableView.vue'),
+      component: () =>
+        import(/* @vite-ignore */ '@/views/Demo2DroppableView.vue'),
     },
     {
       path: '/demo3Plugins',
       name: 'demo3Plugins',
-      component: () => import('@/views/Demo3PluginsView.vue'),
+      component: () =>
+        import(/* @vite-ignore */ '@/views/Demo3PluginsView.vue'),
     },
     {
       path: '/demo4Sortable',
       name: 'demo4Sortable',
-      component: () => import('@/views/Demo4SortableView.vue'),
+      component: () =>
+        import(/* @vite-ignore */ '@/views/Demo4SortableView.vue'),
     },
     {
       path: '/demo5Swappable',
       name: 'demo5Swappable',
-      component: () => import('@/views/Demo5SwappableView.vue'),
+      component: () =>
+        import(/* @vite-ignore */ '@/views/Demo5SwappableView.vue'),
     },
     {
       path: '/about',
@@ -40,7 +45,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('@/views/AboutView.vue'),
+      component: () => import(/* @vite-ignore */ '@/views/AboutView.vue'),
     },
   ],
 })
