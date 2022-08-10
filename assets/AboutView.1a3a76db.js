571ee528cb45a5e9d5975ad3222ae37f879011ae
ayugioh2003 <ayugioh2003@gmail.com>
2022-08-10 09:12:05 +0000

    Deploying to gh-pages from @ ayugioh2003/survey-draggablejs@e79462941c7b6ae6b32bbf6f10276d4ae53a922d 🚀

--- assets/AboutView.1a3a76db.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{b as o,c as t,d as s}from"./index.572385fe.js";const c={},a={class:"about"},n=s("h1",null,"This is an about page",-1),_=[n];function r(i,d){return o(),t("div",a,_)}const u=e(c,[["render",r]]);export{u as default};
