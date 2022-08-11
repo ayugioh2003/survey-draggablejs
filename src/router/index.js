b91fa2640e297c29175339d68817fb9c2b6e4686
John Chiu <john@makertec.net>
2022-08-11 09:48:56 +0800

    try: router dynamic route

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 // import Demo1View from '@/views/Demo1DraggableView.vue'
-import Demo2View from '@/views/Demo2DroppableView.vue'
-import Demo3View from '@/views/Demo3PluginsView.vue'
-import Demo4View from '@/views/Demo4SortableView.vue'
-import Demo5View from '@/views/Demo5SwappableView.vue'
+// import Demo2View from '@/views/Demo2DroppableView.vue'
+// import Demo3View from '@/views/Demo3PluginsView.vue'
+// import Demo4View from '@/views/Demo4SortableView.vue'
+// import Demo5View from '@/views/Demo5SwappableView.vue'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -27,17 +27,17 @@ const router = createRouter({
     {
       path: '/demo3Plugins',
       name: 'demo3Plugins',
-      component: Demo3View,
+      component: () => import('../views/Demo3PluginsView.vue'),
     },
     {
       path: '/demo4Sortable',
       name: 'demo4Sortable',
-      component: Demo4View,
+      component: () => import('../views/Demo4SortableView.vue'),
     },
     {
       path: '/demo5Swappable',
       name: 'demo5Swappable',
-      component: Demo5View,
+      component: () => import('@/views/Demo5SwappableView.vue'),
     },
   ],
 })
